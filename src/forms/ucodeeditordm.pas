942149ecb9add61f0ca73a7ee7d70abfc25cc66b
Helltar <mail@helltar.com>
2022-03-08 17:21:28 +0200

    update contacts in file headers

--- src/forms/ucodeeditordm.pas
+++ src/forms/ucodeeditordm.pas
@@ -1,6 +1,6 @@
 {-------------------------------------------------------------------------------
 
-Copyright (C) 2015 Taras Adamchuk <helltar.live@gmail.com>
+Copyright (C) 2015 Helltar <mail@helltar.com>
 
 This file is part of AMPASIDE.
 
